2c8ac4748de64e4dbaf7fb7f1e6b982659f100cf
Vazuka <119946173+Vazuka@users.noreply.github.com>
2024-07-16 08:33:16 +0100

    Scraping-1

---
+++ src/scraper.py
@@ -0,0 +1,8 @@
+import time  # For handling time-related functions
+import re  # For regular expressions
+import numpy as np  # For numerical operations
+import pandas as pd  # For data manipulation and analysis
+from bs4 import BeautifulSoup  # For web scraping
+from selenium import webdriver  # For browser automation
+from selenium.webdriver.chrome.service import Service  # For configuring the ChromeDriver service
+
